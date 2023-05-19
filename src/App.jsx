a7a8fce5f4ca6c7e355be82057e37aa16916e4fd
MohammedAbuEssa <mabuissa5@gmail.com>
2023-05-19 10:57:25 +0000

    Fix the routing isse

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbarr from './Components/Navbarr';
 import Home from './Components/Home';
-import { Route,Routes } from 'react-router-dom';
-import MovieList from './Components/MovieList';
+import { BrowserRouter, Route,Routes } from 'react-router-dom';
 import FavList from './Components/FavList';
 
 export default function App() {
   return (
 <>
+<BrowserRouter>
 <Navbarr/>
 <Routes>
   <Route path='/' element={<Home/>}/>
   <Route path='/favlist' element={<FavList/>}/>
 </Routes>
+</BrowserRouter>
 </>
   )}
 
